bf84355f61c3a500cd285704341511b259316993
allekok <one@allekok.com>
2019-12-08 09:16:06 +0330

    * PROG: My programs added.

--- files/.bashrc
+++ files/.bashrc
@@ -1,9 +1,14 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
+# Load aliases
+if [ -f "$HOME/.bash_aliases" ]; then
+    . "$HOME/.bash_aliases"
+fi
+
 PS1='\W \e[93m\$\e[0m '
 EDITOR=emacsclient
-PATH=$PATH:"$HOME/projects/MY-PROGRAMS"
+PATH=$PATH:"$HOME/PROG"
 
 HISTCONTROL=ignoreboth
 HISTSIZE=1000
---
+++ files/PROG/my-library.php
@@ -0,0 +1,33 @@
+<?php
+function nth ($n, $def=false)
+{
+    global $argv;
+    $arg = @$argv[$n];
+    return $arg ? $arg : $def;
+}
+
+function p ($string='')
+{
+    echo "$string\n";
+}
+
+function nth_pair ($key, $def=false)
+{
+    global $argv;
+    $carry = false;
+    foreach ($argv as $o)
+    {
+	if($carry)
+	    return $o;
+	elseif($key == $o)
+	    $carry = true;
+    }
+    return $def;
+}
+
+function smart_pair ($key, $def=false)
+{
+    // Check for all kind of pairs: 
+    // ([-x test], [--x test], [--x=test], [-x=test])
+}
+?>
