a6bba5146a38d0d9f059899c6ebd8daa65a58ff6
Payam <payambapiri.97@gmail.com>
2020-04-30 01:59:40 +0430

    Automatic daily update

--- files/home/.emacs.d/init.el
+++ files/home/.emacs.d/init.el
@@ -18,9 +18,6 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes '(allekok-light))
- '(custom-safe-themes
-   '("1588ba19f6c6b369bb0bb43708c13a43dca0c3761d22efa189ba64766c27b668" "076ed8d6973832d37876d2c2ff72f1b08096a75e64c280147745da403a7a3ba9" allekok-dark allekok-light default))
  '(package-selected-packages '(haskell-mode exwm xelb web-mode markdown-mode async))
  '(send-mail-function 'smtpmail-send-it)
  '(smtpmail-smtp-server "smtp.gmail.com")
