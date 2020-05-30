a68bbfb923738c5c2f611f0d033f1b7c5fa43c97
Payam <payambapiri.97@gmail.com>
2020-05-31 00:59:26 +0430

    Automatic daily update

--- files/home/.emacs.d/init.el
+++ files/home/.emacs.d/init.el
@@ -1,7 +1,16 @@
-(setq gc-cons-threshold (* 150 1024 1024))
+(setq gc-cons-threshold (* 100 1024 1024))
 
 (load (expand-file-name "config" user-emacs-directory))
 
 (custom-set-variables
- '(package-selected-packages
-   '(markdown-mode exwm xelb web-mode async)))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(markdown-mode exwm xelb web-mode async)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
