6ac57716f821c95c5d8ec6046cbfcb29cf16b9d3
Payam <payambapiri.97@gmail.com>
2020-07-25 23:53:28 +0430

    Automatic daily update

--- files/home/PROG/my-library.php
+++ files/home/PROG/my-library.php
@@ -1,33 +1,34 @@
 <?php
 function nth ($n, $def=false)
 {
-    global $argv;
-    $arg = @$argv[$n];
-    return $arg ? $arg : $def;
+	global $argv;
+	$arg = @$argv[$n];
+	return $arg ? $arg : $def;
 }
 
 function p ($string='')
 {
-    echo "$string\n";
+	echo "$string\n";
 }
 
 function nth_pair ($key, $def=false)
 {
-    global $argv;
-    $carry = false;
-    foreach ($argv as $o)
-    {
-	if($carry)
-	    return $o;
-	elseif($key == $o)
-	    $carry = true;
-    }
-    return $def;
+	global $argv;
+	$carry = false;
+	foreach ($argv as $o)
+	{
+		if($carry)
+			return $o;
+		elseif($key == $o)
+			$carry = true;
+	}
+	return $def;
 }
 
 function smart_pair ($key, $def=false)
 {
-    // Check for all kind of pairs: 
-    // ([-x test], [--x test], [--x=test], [-x=test])
+	// Check for all kind of pairs: 
+	// ([-x test], [--x test], [--x=test], [-x=test])
+	// Is it possible?
 }
 ?>
