fef1c0c2a9701674ff4896aca7f7e6a8803a0c8d
Payam <payambapiri.97@gmail.com>
2020-08-03 23:22:09 +0430

    Automatic daily update

--- files/home/.emacs.d/init.el
+++ files/home/.emacs.d/init.el
@@ -11,7 +11,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(markdown-mode exwm xelb web-mode async)))
+ '(package-selected-packages
+   '(csharp-mode google-translate markdown-mode exwm xelb web-mode async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
