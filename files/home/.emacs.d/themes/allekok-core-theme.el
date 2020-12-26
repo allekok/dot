ccd26d66c62f6faa77f41cc25350b475322caf57
Payam <payambapiri.97@gmail.com>
2020-12-26 19:20:34 +0330

    Automatic daily update

--- files/home/.emacs.d/themes/allekok-core-theme.el
+++ files/home/.emacs.d/themes/allekok-core-theme.el
@@ -1,15 +1,86 @@
 (defun get-colorname (LD)
-  (setenv "COLORNOW" LD)
-  (let ((colors
-	 (split-string
-	  (with-temp-buffer
-	    (insert-file-contents
-	     (expand-file-name
-	      (if (string= LD "dark")
-		  ".color-dark" ".color-light")
-	      (getenv "HOME")))
-	    (buffer-string))
-	  split-string-default-separators t)))
-    colors))
+  (let ((color (if (eq LD 'allekok-dark)
+		   "dark" "light")))
+    (setenv "COLORNOW" color)
+    (let ((colors
+	   (split-string
+	    (with-temp-buffer
+	      (insert-file-contents
+	       (expand-file-name
+		(format ".color-%s" color)
+		(getenv "HOME")))
+	      (buffer-string))
+	    split-string-default-separators t)))
+      colors)))
 
-(provide 'allekok-core-theme)
+(let* ((class '((class color) (min-colors 89)))
+       ;; Font
+       (font-family "SourceCodePro")
+       (font-size-main 110)
+       (font-size-mini 105)
+       ;; Color
+       (colors (get-colorname allekok-theme))
+       (back-color (nth 0 colors))
+       (fore-color (nth 1 colors))
+       (keyword-color (nth 2 colors))
+       (warn-color (nth 3 colors)))
+  (custom-theme-set-faces
+   allekok-theme
+   `(default ((,class (:family
+		       ,font-family
+		       :background ,back-color
+		       :foreground ,fore-color
+		       :height ,font-size-main
+		       :weight bold))))
+   `(mode-line ((,class (:foreground
+			 ,fore-color
+			 :background ,back-color
+			 :height ,font-size-mini
+			 :weight bold
+			 :box (:color
+			       ,back-color
+			       :line-width 2)))))
+   `(mode-line-inactive ((,class (:foreground
+				  ,fore-color
+				  :background ,back-color
+				  :height ,font-size-mini
+				  :weight bold
+				  :box (:color
+					,back-color
+					:line-width 2)))))
+   `(cursor ((,class (:background ,keyword-color))))
+   `(region ((,class (:background ,keyword-color :foreground ,back-color))))
+   `(highlight ((,class (:background ,keyword-color :foreground ,back-color))))
+   `(hl-line ((,class (:background ,back-color))))
+   `(vertical-border ((,class (:foreground ,keyword-color))))
+   `(show-paren-match ((,class (:foreground ,keyword-color
+					    :weight bold))))
+   `(font-lock-warning-face ((,class (:foreground ,warn-color))))
+   `(font-lock-keyword-face ((,class (:foreground ,keyword-color))))
+   `(font-lock-builtin-face ((,class (:foreground ,fore-color))))
+   `(font-lock-function-name-face ((,class (:foreground ,keyword-color :weight bold))))
+   `(font-lock-string-face ((,class :foreground ,fore-color)))
+   `(font-lock-comment-face ((,class :background ,back-color)))
+   `(font-lock-variable-name-face ((,class :foreground ,fore-color)))
+   `(font-lock-constant-face ((,class :foreground ,keyword-color :weight bold)))
+   `(minibuffer-prompt ((,class :foreground ,keyword-color)))
+   `(link ((,class (:foreground ,keyword-color :weight bold))))
+   `(header-line ((,class (:background ,back-color))))
+   `(eww-valid-certificate ((,class (:foreground ,keyword-color :weight bold))))
+   `(isearch ((,class (:underline t))))
+   `(isearch-fail ((,class (:strike-through t))))
+   `(lazy-highlight ((,class (:underline t :foreground ,keyword-color))))
+   `(eshell-prompt ((,class (:foreground ,keyword-color :weight bold))))
+   `(dired-async-message ((,class (:foreground ,keyword-color))))
+   `(web-mode-html-tag-face ((,class (:foreground ,fore-color))))
+   `(web-mode-html-attr-name-face ((,class (:foreground ,fore-color))))
+   `(error ((,class (:foreground ,warn-color))))
+   `(compilation-info ((,class (:foreground ,keyword-color))))
+   `(org-todo ((,class (:foreground ,warn-color))))
+   `(org-done ((,class (:foreground ,keyword-color))))
+   `(font-lock-type-face ((,class (:foreground ,fore-color))))
+   `(org-document-title ((,class (:foreground ,keyword-color))))
+   `(org-document-info ((,class (:foreground ,keyword-color))))
+   `(erc-notice-face ((,class (:foreground ,keyword-color))))))
+
+(provide-theme allekok-theme)
--- files/home/.emacs.d/themes/allekok-light-theme.el
+++ files/home/.emacs.d/themes/allekok-light-theme.el
@@ -3,78 +3,9 @@
 ;; Author: Payam <payambapiri.97@gmail.com>
 
 ;;; Code:
-(require 'allekok-core-theme)
-
 (deftheme allekok-light)
 
-(let* ((class '((class color) (min-colors 89)))
-       ;; Font
-       (font-family "NotoSansMono")
-       (font-size-main 110)
-       (font-size-mini 110)
-       ;; Color
-       (colors (get-colorname "light"))
-       (back-color (nth 0 colors))
-       (fore-color (nth 1 colors))
-       (keyword-color (nth 2 colors))
-       (warn-color (nth 3 colors)))
-  (custom-theme-set-faces
-   'allekok-light
-   `(default ((,class (:family
-		       ,font-family
-		       :background ,back-color
-		       :foreground ,fore-color
-		       :height ,font-size-main
-		       :weight medium))))
-   `(mode-line ((,class (:foreground
-			 ,fore-color
-			 :background ,back-color
-			 :height ,font-size-mini
-			 :weight medium
-			 :box (:color
-			       ,back-color
-			       :line-width 2)))))
-   `(mode-line-inactive ((,class (:foreground
-				  ,fore-color
-				  :background ,back-color
-				  :height ,font-size-mini
-				  :weight medium
-				  :box (:color
-					,back-color
-					:line-width 2)))))
-   `(cursor ((,class (:background ,keyword-color))))
-   `(region ((,class (:background ,keyword-color :foreground ,back-color))))
-   `(highlight ((,class (:background ,keyword-color :foreground ,back-color))))
-   `(hl-line ((,class (:background ,back-color))))
-   `(vertical-border ((,class (:foreground ,keyword-color))))
-   `(show-paren-match ((,class (:foreground ,keyword-color
-					    :weight normal))))
-   `(font-lock-warning-face ((,class (:foreground ,warn-color))))
-   `(font-lock-keyword-face ((,class (:foreground ,keyword-color))))
-   `(font-lock-builtin-face ((,class (:foreground ,fore-color))))
-   `(font-lock-function-name-face ((,class (:weight normal))))
-   `(font-lock-string-face ((,class :foreground ,fore-color)))
-   `(font-lock-comment-face ((,class :background ,back-color)))
-   `(font-lock-variable-name-face ((,class :foreground ,fore-color)))
-   `(font-lock-constant-face ((,class :weight normal)))
-   `(minibuffer-prompt ((,class :foreground ,keyword-color)))
-   `(link ((,class (:foreground ,keyword-color :weight bold))))
-   `(header-line ((,class (:background ,back-color))))
-   `(eww-valid-certificate ((,class (:foreground ,keyword-color :weight normal))))
-   `(isearch ((,class (:underline t))))
-   `(isearch-fail ((,class (:strike-through t))))
-   `(lazy-highlight ((,class (:underline t :foreground ,keyword-color))))
-   `(eshell-prompt ((,class (:foreground ,keyword-color :weight medium))))
-   `(dired-async-message ((,class (:foreground ,keyword-color))))
-   `(web-mode-html-tag-face ((,class (:foreground ,fore-color))))
-   `(web-mode-html-attr-name-face ((,class (:foreground ,fore-color))))
-   `(error ((,class (:foreground ,warn-color))))
-   `(compilation-info ((,class (:foreground ,keyword-color))))
-   `(org-todo ((,class (:foreground ,warn-color))))
-   `(org-done ((,class (:foreground ,keyword-color))))
-   `(font-lock-type-face ((,class (:foreground ,fore-color))))
-   `(erc-notice-face ((,class (:foreground ,keyword-color))))))
-
-(provide-theme 'allekok-light)
+(setq allekok-theme 'allekok-light)
+(load "allekok-core-theme")
 
 ;;; allekok-light-theme.el  ends here
