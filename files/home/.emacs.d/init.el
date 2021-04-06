2bb428026eadc3f2a856e4371ddc8a45018ae961
Payam <payambapiri.97@gmail.com>
2021-04-06 21:55:00 +0430

    Automatic daily update

--- files/home/.emacs.d/init.el
+++ files/home/.emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-bullets-mode elisp--witness--lisp org-bullets markdown-mode exwm xelb web-mode async)))
+   '(go-mode org-bullets-mode elisp--witness--lisp org-bullets markdown-mode exwm xelb web-mode async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
