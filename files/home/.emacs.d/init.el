466a8ab442f8ff72f88d1a625c8a637a9714b5d8
Payam <payambapiri.97@gmail.com>
2021-08-26 02:35:41 +0430

    Automatic daily update

--- files/home/.emacs.d/init.el
+++ files/home/.emacs.d/init.el
@@ -9,7 +9,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(go-mode elisp--witness--lisp markdown-mode exwm xelb web-mode)))
+   '(go-mode elisp--witness--lisp markdown-mode exwm xelb web-mode))
+ '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
