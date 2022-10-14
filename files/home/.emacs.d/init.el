92fe8c71e82fff074c63b42f40ec33ab946d3da4
Payam <payambapiri.97@gmail.com>
2022-10-14 03:38:58 +0330

    Automatic daily update

--- files/home/.emacs.d/init.el
+++ files/home/.emacs.d/init.el
@@ -7,6 +7,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages '(exwm web-mode elisp--witness--lisp))
+ '(safe-local-variable-values
+   '((diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w")
+     (bidi-paragraph-direction . right-to-left)))
  '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
